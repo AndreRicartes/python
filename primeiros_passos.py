19362028c271c52937f7af2a4b8b062fd50f3739
André Ricartes <andrericartes@hotmail.com>
2023-05-09 17:18:49 -0300

    Update primeiros_passos.py

--- primeiros_passos.py
+++ primeiros_passos.py
@@ -1,5 +1,22 @@
 #Comando para saída de dados
 print("Olá Mundo!")
 
+#Entrar com 'n' valores e mostrar o menor
+
+n = float(input("Quantos valores serão inseridos? "))
+cont = 0
+
+menor = valor
+while cont < n:
+    valor = float(input("Entre com o valor: "))
+    cont += 1
+    if menor > valor:
+        menor = valor
+    print(valor)
+print(menor)
+
+
+
+
 
 
